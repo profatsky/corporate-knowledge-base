f1ec658bb1176a3a1568bc9376228d55ef902f2a
Mikhail Profatsky <mixail.rostov04@gmail.com>
2022-12-22 20:59:03 +0300

    added catalog and document models to admin panel, added verbose name for materials app in admin panel

--- src/materials/admin.py
+++ src/materials/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Catalog, Document
+
+
+class DocumentInline(admin.TabularInline):
+    model = Document
+
+
+@admin.register(Catalog)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id',)
+    fields = ('name',)
+    search_fields = ('name',)
+    inlines = [DocumentInline]
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'catalog', 'is_private', 'disk_path',)
+    list_display_links = ('id', 'name',)
+    list_editable = ('is_private',)
+    list_filter = ('catalog', 'is_private')
+    fields = ('catalog', 'name', 'is_private', 'disk_path',)
+    search_fields = ('name',)
+
+
+admin.site.site_header = 'Администрирование Корпоративной базы знаний'
