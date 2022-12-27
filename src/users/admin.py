7c3d6aa9ce7391d9a9144c85f1cf508949d1236a
Mikhail Profatsky <mixail.rostov04@gmail.com>
2022-12-27 18:53:12 +0300

    updated user creation via admin panel, the generated password is sent to the new user's mail

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.admin import StackedInline
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.core.mail import send_mail
+from django.utils.crypto import get_random_string
 
+from staff.models import Employee
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
+
+
+class ProfileInline(StackedInline):
+    model = Employee
 
 
 @admin.register(User)
@@ -12,21 +21,36 @@ class UserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = User
 
-    list_display = ('email', 'is_staff', 'is_active', 'date_joined', 'last_login',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('id', 'email', 'is_staff', 'is_active', 'date_joined', 'last_login',)
+    list_display_links = ('id', 'email',)
+    list_filter = ('is_staff', 'is_active',)
     search_fields = ('email',)
-    ordering = ('email',)
+    ordering = ('id',)
+    inlines = [ProfileInline]
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('УЧЕТНАЯ ЗАПИСЬ', {'fields': ('email', 'password', 'is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'is_staff', 'is_active')}
          ),
     )
+
+    def save_model(self, request, obj, form, change):
+        if not change and (not form.cleaned_data['password1'] or not obj.has_usable_password()):
+            password = get_random_string(length=12)
+            obj.set_password(password)
+            send_mail(
+                subject='Корпоративная база знаний',
+                message='Вы зарегистрированы в системе Корпоративной базы знаний\n'
+                        f'Ваш логин - {obj.email}, пароль - {password}',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[obj.email],
+            )
+
+        super().save_model(request, obj, form, change)
 
 
 admin.site.unregister(Group)
--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 from django.db import models
 
 from .managers import UserManager
@@ -12,6 +15,20 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        if not self.password:
+            password = User.objects.make_random_password()
+            self._password = password
+            self.password = make_password(password)
+            send_mail(
+                subject='Корпоративная база знаний',
+                message='Вы зарегистрированы в системе Корпоративной базы знаний\n'
+                        f'Ваш логин - {self.email}, пароль - {self._password}',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[self.email],
+            )
+        super().save()
 
     def __str__(self):
         return self.email
